148856979bd3be4ac65473adf0aecf2fec0f6232
Yoshiaki Kawazu <kawazzz@gmail.com>
2021-07-14 12:28:27 +0900

    update new cue

--- config/config-scheme.cue
+++ config/config-scheme.cue
@@ -17,3 +17,74 @@ Memcached?: [
   ...( string & =~ "^[a-z0-9_\\.-]+:[0-9]+$" )
 ]
 CacheTTL?: time.Duration | *120
+// Listeners: [#Listener, ...#Listener]
+// Routes: [...#Route]
+// Backends: [string]: #Backend
+
+Port: uint16
+
+//EndPoints: #Endpoint
+
+#Listener: {
+	Port: uint16
+	Middlewares: [...#Middleware]
+}
+
+#Middleware: {
+	Type: string,
+	Disabled: bool | *false,
+	Middlewares?: [...#Middleware]
+	...
+}
+
+#Route: {
+	Backend: string
+	...
+}
+
+#Filter: Matcher: #Matcher
+
+#Matcher: string
+
+//Backends: [_Name=@]: {Name: _Name}
+#Backend: {
+	Type: string
+} & ( #BackendProxy | #BackendMock | #BackendStatic )
+
+#BackendProxy: {
+	Type: "Proxy"
+	Url:  string
+}
+#BackendMock: {
+	Type: "Mock"
+	Routes: [#Method]: {
+		Code:  int & >=100 & <600
+		Tyep?: "text/plain; charset=UTF-8"
+		Body:  string
+	}
+}
+#BackendStatic: {
+	Type:         "Static"
+	DocumentRoot: string
+}
+
+#HealthCheck: #HttpHealthcheck | #TcpHealthcheck | #PingHealthcheck
+#HttpHealthcheck: {
+	Path:           string
+	Code:           int
+	HealthyCount:   int | *1
+	UhhealtyCount:  int | *2
+	Interval:       time.Duration | *"30s"
+	Timeout:        time.Duration | *"5s"
+	ConnectTimeout: time.Duration | *"3s"
+}
+#TcpHealthcheck: {
+	ConnectTimeout: time.Duration | *"3s"
+	Send?:          string | bytes
+	Reponse?:       string | bytes
+}
+#PingHealthcheck: {
+	Timeout: time.Duration | *"3s"
+}
+
+#Method: *"GET" | "POST" | "HEAD" | "OPTIONS" | "POST" | "PUT" | "DELETE" | "PATCH"
--- config/config.go
+++ config/config.go
@@ -4,19 +4,16 @@ import (
 	_ "embed"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"cuelang.org/go/cue"
-	cueerrors "cuelang.org/go/cue/errors"
-	"cuelang.org/go/encoding/gocode/gocodec"
+	"cuelang.org/go/cue/cuecontext"
 )
 
-var r cue.Runtime
-
-//go:embed "config-scheme.cue"
 var configScheme []byte
 
 type Config struct {
@@ -29,34 +26,87 @@ type Config struct {
 }
 
 func Load(files ...string) (*Config, error) {
-	var merged *cue.Instance
-	scheme, err := r.Compile("github.com/kawaz/go-zunproxy/config/config-scheme.cue", configScheme)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse cue: %v", err)
+	cctx := cuecontext.New()
+	cfg := cctx.CompileBytes(configScheme)
+	if cfg.Err() != nil {
+		return nil, cfg.Err()
 	}
-	merged = scheme
-	for _, file := range files {
-		newInstance, err := loadFile(file)
-		if err != nil {
-			return nil, fmt.Errorf("could not load cue: %v", cueerrors.Details(err, nil))
-		}
-		if merged == nil {
-			merged = newInstance
-		}
-		merged = cue.Merge(merged, newInstance)
 
-		err = merged.Value().Validate()
+	for _, f := range files {
+		src, err := ioutil.ReadFile(f)
 		if err != nil {
 			return nil, err
 		}
+		v := cctx.CompileBytes(src)
+		if v.Err() != nil {
+			return nil, v.Err()
+		}
+		cfg = cfg.Unify(v)
 	}
-	c := Config{}
-	codec := gocodec.New(&r, &gocodec.Config{})
-	err = codec.Encode(merged.Value(), &c)
+	err := cfg.Validate()
 	if err != nil {
-		return nil, err
+		panic(fmt.Errorf("cfg is not valid: %w", err))
 	}
-	return &c, nil
+	if cfg.Err() != nil {
+		panic(cfg.Err())
+	}
+	// pp.Println(cfg)
+	c := &Config{}
+	err = cfg.Decode(c)
+	if err != nil {
+		panic(fmt.Errorf("can not decode: %w", err))
+	}
+	return c, nil
+	// fields, err := cfg.Fields()
+	// pp.Println(fields, err)
+	// for next := fields.Next(); next; next = fields.Next() {
+	// 	pp.Print("f", fields.Value())
+	// }
+	// cctx := cuecontext.New()
+	// cfgscheme := cctx.CompileBytes(configScheme)
+	// pp.Println(cfgscheme)
+	// pp.Println(cfgscheme.Err())
+	// cctx.BuildInstance()
+
+	// c := &Config{}
+	// enc := cctx.Encode(c)
+	// pp.Println("c", c)
+	// pp.Println("cctx.Encode(&c)", enc)
+	// cfgscheme.Context().CompileBytes(nil)
+	// err = cfgscheme.Decode(&c)
+	// pp.Println("scheme.Decode(&c)", err)
+	// pp.Println("c", c)
+	// // scheme.Format(fmt.State(""), ' ')
+	// cue.Merge()
+	// // var merged *cue.Instance
+
+	// scheme, err := r.Compile("github.com/kawaz/go-zunproxy/config/config-scheme.cue", configScheme)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("could not parse cue: %v", err)
+	// }
+	// merged = scheme
+	// for _, file := range files {
+	// 	newInstance, err := loadFile(file)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("could not load cue: %v", cueerrors.Details(err, nil))
+	// 	}
+	// 	if merged == nil {
+	// 		merged = newInstance
+	// 	}
+	// merged = cue.Merge(merged, newInstance)
+
+	// 	err = merged.Value().Validate()
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// }
+	// c := Config{}
+	// codec := gocodec.New(&r, &gocodec.Config{})
+	// err = codec.Encode(merged.Value(), &c)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return &c, nil
 }
 
 func loadFile(path string) (*cue.Instance, error) {
@@ -80,7 +130,7 @@ func loadFile(path string) (*cue.Instance, error) {
 }
 
 func loadCUE(path string, file *os.File) (*cue.Instance, error) {
-	return r.Compile(path, file)
+	return nil, nil //r.Compile(path, file)
 }
 
 func loadJSON(path string, reader io.Reader) (*cue.Instance, error) {
